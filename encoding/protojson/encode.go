ae3153a68b3bb892137346e7e5bfb6a958f56b8f
TaehyungLee <taehyung.lee@devsisters.com>
2024-06-13 21:47:15 +0900

    int64 를 int 로 encode

--- encoding/protojson/encode.go
+++ encoding/protojson/encode.go
@@ -309,16 +309,26 @@ func (e encoder) marshalSingular(val protoreflect.Value, fd protoreflect.FieldDe
 			return errors.InvalidUTF8(string(fd.FullName()))
 		}
 
-	case protoreflect.Int32Kind, protoreflect.Sint32Kind, protoreflect.Sfixed32Kind:
+	//case protoreflect.Int32Kind, protoreflect.Sint32Kind, protoreflect.Sfixed32Kind:
+	//	e.WriteInt(val.Int())
+	//
+	//case protoreflect.Uint32Kind, protoreflect.Fixed32Kind:
+	//	e.WriteUint(val.Uint())
+	//
+	//case protoreflect.Int64Kind, protoreflect.Sint64Kind, protoreflect.Uint64Kind,
+	//	protoreflect.Sfixed64Kind, protoreflect.Fixed64Kind:
+	//	// 64-bit integers are written out as JSON string.
+	//	e.WriteString(val.String())
+
+	// int64, uint64 는 String 으로 encode 되는게 표준이지만, protojson 을 로깅에만 쓰므로 int 로 encode
+	case protoreflect.Int32Kind, protoreflect.Sint32Kind, protoreflect.Sfixed32Kind,
+		protoreflect.Int64Kind, protoreflect.Sint64Kind, protoreflect.Sfixed64Kind:
 		e.WriteInt(val.Int())
 
-	case protoreflect.Uint32Kind, protoreflect.Fixed32Kind:
+	case protoreflect.Uint32Kind, protoreflect.Fixed32Kind,
+		protoreflect.Uint64Kind, protoreflect.Fixed64Kind:
 		e.WriteUint(val.Uint())
-
-	case protoreflect.Int64Kind, protoreflect.Sint64Kind, protoreflect.Uint64Kind,
-		protoreflect.Sfixed64Kind, protoreflect.Fixed64Kind:
-		// 64-bit integers are written out as JSON string.
-		e.WriteString(val.String())
+	//
 
 	case protoreflect.FloatKind:
 		// Encoder.WriteFloat handles the special numbers NaN and infinites.
